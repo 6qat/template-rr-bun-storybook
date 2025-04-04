1803abff134aa19f19877ffa1b9c57349167cdca
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2025-04-04 10:06:32 -0300

    Update packages

--- server-elysia.ts
+++ server-elysia.ts
@@ -1,26 +1,24 @@
 // https://github.com/kravetsone/elysia-react-router/blob/main/example/server/index.ts
 
-import { Elysia } from 'elysia';
-import { reactRouter } from 'elysia-react-router';
+import { Elysia } from "elysia";
+import { reactRouter } from "elysia-react-router";
 
 const port = Number(process.env.PORT) || 3000;
 
 new Elysia()
   .use(
     await reactRouter({
-      getLoadContext: () => ({ hotPostName: 'some post title' }),
+      getLoadContext: () => ({ hotPostName: "some post title" }),
     })
   )
-  .get('/some', 'Hello')
-  .get('/installHook.js.map', '')
-  .get('/%3Canonymous%20code%3E', '')
+
   .listen(port, () => {
     console.log(
       `Elysia server is running on ${process.env.NODE_ENV} mode at http://localhost:${port}`
     );
   });
 
-declare module 'react-router' {
+declare module "react-router" {
   interface AppLoadContext {
     hotPostName: string;
   }
